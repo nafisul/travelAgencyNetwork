c59fcadf59bd91018c9bfb91d4465e560aa355a4
Mahtab Uddin Shahed <shahed.blackmagic@gmail.com>
2019-07-20 01:44:01 +0600

    Initial project

---
+++ src/main/java/com/bs23/travelagencynetwork/TravelagencynetworkApplication.java
@@ -0,0 +1,15 @@
+package com.bs23.travelagencynetwork;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravelagencynetworkApplication {
+	private static final Logger logger = LogManager.getLogger(TravelagencynetworkApplication.class);
+	public static void main(String[] args) {
+		logger.info("this is mahtab uddin shahed");
+		SpringApplication.run(TravelagencynetworkApplication.class, args);
+	}
+}
