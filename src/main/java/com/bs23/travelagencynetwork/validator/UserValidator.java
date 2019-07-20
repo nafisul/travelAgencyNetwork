8e7206402687e3f085ebecf89af5460dd07a6842
Mahtab Uddin Shahed <shahed.blackmagic@gmail.com>
2019-07-20 19:06:00 +0600

    user registration validation updated

--- src/main/java/com/bs23/travelagencynetwork/validator/UserValidator.java
+++ src/main/java/com/bs23/travelagencynetwork/validator/UserValidator.java
@@ -1,6 +1,8 @@
 package com.bs23.travelagencynetwork.validator;
 
 import com.bs23.travelagencynetwork.entity.User;
+import com.bs23.travelagencynetwork.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.validation.Errors;
 import org.springframework.validation.ValidationUtils;
@@ -8,6 +10,10 @@ import org.springframework.validation.Validator;
 
 @Component
 public class UserValidator implements Validator {
+
+    @Autowired
+    private UserService userService;
+
     @Override
     public boolean supports(Class<?> aClass) {
         return User.class.equals(aClass);
@@ -21,9 +27,19 @@ public class UserValidator implements Validator {
         if (user.getName().length() < 6 || user.getName().length() > 32) {
             errors.rejectValue("name", "Size.userForm.name");
         }
-        /*if (userService.findByUsername(user.getUsername()) != null) {
-            errors.rejectValue("username", "Duplicate.userForm.username");
-        }*/
+
+        if (userService.findByEmail(user.getEmail()) != null) {
+            errors.rejectValue("email", "Duplicate.userForm.email");
+        }
+
+        ValidationUtils.rejectIfEmptyOrWhitespace(errors, "password", "NotEmpty");
+        if (user.getPassword().length() < 8 || user.getPassword().length() > 32) {
+            errors.rejectValue("password", "Size.userForm.password");
+        }
+
+        if (!user.getPasswordConfirm().equals(user.getPassword())) {
+            errors.rejectValue("passwordConfirm", "Diff.userForm.passwordConfirm");
+        }
 
     }
 }
--- src/main/webapp/WEB-INF/views/registration.jsp
+++ src/main/webapp/WEB-INF/views/registration.jsp
@@ -29,7 +29,7 @@
         </spring:bind>
 
         <spring:bind path="email">
-            <div class="form-group <%--${status.error ? 'has-error' : ''}--%>">
+            <div class="form-group ${status.error ? 'has-error' : ''}">
                 <form:input type="text" path="email" class="form-control" placeholder="Email Address"
                             autofocus="true"></form:input>
                 <form:errors path="email"></form:errors>
@@ -37,14 +37,14 @@
         </spring:bind>
 
         <spring:bind path="password">
-            <div class="form-group <%--${status.error ? 'has-error' : ''}--%>">
+            <div class="form-group ${status.error ? 'has-error' : ''}">
                 <form:input type="password" path="password" class="form-control" placeholder="Password"></form:input>
                 <form:errors path="password"></form:errors>
             </div>
         </spring:bind>
 
         <spring:bind path="passwordConfirm">
-            <div class="form-group <%--${status.error ? 'has-error' : ''}--%>">
+            <div class="form-group ${status.error ? 'has-error' : ''}">
                 <form:input type="password" path="passwordConfirm" class="form-control"
                             placeholder="Confirm your password"></form:input>
                 <form:errors path="passwordConfirm"></form:errors>
